9de7f20a84a3387aceb19b5b9a31d3f43d90566d
Tango <53480103+tango-del@users.noreply.github.com>
2021-02-11 13:55:32 +0200

    make realization createRawProduct in DatabaseRequests

--- src/main/java/DB_product_orders/DatabaseRequests.java
+++ src/main/java/DB_product_orders/DatabaseRequests.java
@@ -1,9 +1,16 @@
 package DB_product_orders;
 
+import Entities.Product;
+import Enums.ProductStatus;
+import org.hibernate.Query;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class DatabaseRequests {
+    private Scanner scanner;
+    private static Query query;
     // хранит полную локальную дату времени компьютера
     private static final LocalDateTime currentDateTime = LocalDateTime.now();
     // форматирует локальную дату по указанным ключам
@@ -12,7 +19,29 @@ public class DatabaseRequests {
     private static final String dateTime = currentDateTime.format(formatter);
 
     public void createRawProduct() {
-        System.out.println("createRawProduct method");
+        Product testProduct = new Product();
+
+        scanner = new Scanner(System.in);
+
+        System.out.println("Choose Product Name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Choose Product price (numbers)");
+        int productPrice = scanner.nextInt();
+
+        System.out.println("Choose Product Status");
+        int productStatus = chooseProductStatus();
+
+        scanner.close();
+
+        testProduct.setPrice(productPrice);
+        testProduct.setName(name);
+        testProduct.setStatus(productStatus);
+        testProduct.setDataCreate(dateTime);
+
+        Connect.session.beginTransaction();
+        Connect.session.save(testProduct);
+        Connect.session.getTransaction().commit();
     }
 
     public void createOrder() {
@@ -21,5 +50,20 @@ public class DatabaseRequests {
 
     public void updateOrderEntryQuantity() {
         System.out.println("updateOrderEntryQuantity method");
+    }
+
+    private int chooseProductStatus() {
+        System.out.println("1 - out_of_stock");
+        System.out.println("2 - in_stock");
+        System.out.println("3 - running_low");
+
+        int productStatus = scanner.nextInt();
+        switch (productStatus) {
+            case 1 :
+            case 2 :
+            case 3 :
+                return productStatus;
+            default: throw new RuntimeException("incorrect choose product status");
+        }
     }
 }
